75c0ec12e20bda01ee472af07490c75c103df604
johnmark114 <johnmarkogbuinya952@gmail.com>
2025-09-24 17:06:12 +0100

    ci/cid pipeline restart

--- eks-vpc.tf
+++ eks-vpc.tf
@@ -21,10 +21,10 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
   }
     private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
   }
 }
